31fc2b02d21c9a82b8c62f3fde7e8795a1780dad
Tushar V Shelke <109097998+tusharlikestocode@users.noreply.github.com>
2024-05-06 15:34:22 +0530

    added theme for the whole app

---
+++ JobEngine/src/theme/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({      
+  typography: {
+    fontFamily: [
+        'Lexend'
+      ].join(','),
+    button: {
+      textTransform: 'none'
+      
+    }
+   
+
+  }
+});
+
+export default theme;
